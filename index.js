163fd15a7994006bfe465ed0e3337e6b00d2ad9f
Luis Abreu <lmjabreu@gmail.com>
2013-08-03 11:51:22 +0100

    v1

---
+++ index.js
@@ -0,0 +1,73 @@
+var request   = require( 'request' ),
+    cheerio   = require( 'cheerio' ),
+    async     = require( 'async' ),
+    fs        = require( 'fs' ),
+    BASE_URL  = 'http://getmentalnotes.com',
+    CARDS_URL = BASE_URL + '/cards',
+    cards     = [];
+
+function extractCardID ( href ) {
+  return href.match( /.*\/(.*)$/ )[ 1 ];
+}
+
+function extractElementTextIterator ( index, element ) {
+  return cheerio( this ).text();
+}
+
+function extractCardIDFromElementIterator ( index, element ) {
+  return extractCardID( cheerio( this ).attr( 'href' ) );
+}
+
+function parseCardPage ( callback, error, response, body ) {
+  if ( error || response.statusCode !== 200 ) { callback( error ); return; }
+
+  var $cardDetail = cheerio( body ).find( '#cardDetail' );
+
+  cards.push( {
+    id: extractCardID( response.request.href ),
+    title: $cardDetail.find( '.info h1' ).text(),
+    summary: $cardDetail.find( '.info h2' ).text(),
+    description: $cardDetail.find( '.content .how' ).next( 'p' ).text(),
+    examples: $cardDetail.find( '.examples' ).next( 'ul' ).find( 'li' ).map( extractElementTextIterator ),
+    categories: '',
+    related: $cardDetail.find( '.seeAlso a' ).map( extractElementTextIterator ),
+    resources: $cardDetail.find( '.resources p' ).text()
+  } );
+
+  callback();
+}
+
+// Parse card page and push result into cards array
+function loadCardPage ( cardID, callback ) {
+  console.log( 'Processing: ', cardID );
+  request( [ CARDS_URL, cardID ].join( '/' ), parseCardPage.bind( this, callback ) );
+}
+
+// Parse CardIDs and populate the queue
+function cardsPageLoaded ( error, response, body ) {
+  if ( error || response.statusCode !== 200 ) { console.error( error ); return; }
+
+  var $ = cheerio.load( body );
+
+  // Populate the queue
+  queue.push( $( '#cardList .card' ).map( extractCardIDFromElementIterator ) );
+}
+
+// Write contents of cards array to disk
+function writeCardsJSON () {
+  if ( cards.length != 53 ) { console.error( 'Failed to load all 53 cards!' ); return; }
+  fs.writeFile( 'cards.json', JSON.stringify( cards, null, 2 ), { encoding: 'utf-8' },
+    function fileWriteCallback ( error ) {
+      if ( error ) { console.error( error ); }
+      console.log( 'written ' + cards.length + ' cards to cards.json' );
+    }
+  );
+}
+
+// Setup queue
+var queue = async.queue( loadCardPage, 20 );
+
+// When all cards have been pushed to the deck
+queue.drain = writeCardsJSON;
+
+request( CARDS_URL, cardsPageLoaded );
